babe35eab95622c82254c12a22f255d755dc2012
Harshith Bezawada <harsh20y@gmail.com>
2024-09-01 18:27:48 +0530

    server

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../frontend/views'));
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true, // This is deprecated and can be removed
-  useUnifiedTopology: true, // This is deprecated and can be removed
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  tlsInsecure: true, // If you're testing and need to bypass SSL issues, use this cautiously
+  tlsAllowInvalidCertificates: true, // This might help with self-signed certificates
+  tlsAllowInvalidHostnames: true // If you're facing hostname mismatches
+});
+
 // Utility function to format price
 function formatPrice(price) {
   return `$${(price / 100).toFixed(2)}`;
